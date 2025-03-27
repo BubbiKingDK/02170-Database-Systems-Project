f8d547a20e0a6a9d51b633d7bee91e2a7a149af9
SquirrelModeller <squirrelmodeller@gmail.com>
2025-03-27 11:26:33 +0100

    feat: borrow book procedure

--- src/Queries.sql
+++ src/Queries.sql
@@ -1,14 +1,14 @@
 SELECT
-	SECTION.Genre,
+	Section.Genre,
     COUNT(DISTINCT activity_id) AS activity_count,
     COUNT(DISTINCT book_serial_number) AS book_count
-FROM SECTION
+FROM Section
 NATURAL LEFT JOIN Activity
-NATURAL LEFT JOIN book
+NATURAL LEFT JOIN Book
 GROUP BY Genre;
 
 SELECT book_serial_number, title, author
-FROM BOOK
+FROM Book
 WHERE Genre = 'Science Fiction'
 AND book_serial_number NOT IN (SELECT book_serial_number FROM Borrows);
 
@@ -25,8 +25,8 @@ CREATE FUNCTION Availability (vGenre VARCHAR(255)) RETURNS VARCHAR(255)
 BEGIN
 DECLARE vTotalCount INT;
 DECLARE vBorrowCount INT;
-SELECT COUNT(*) INTO vTotalCount FROM BOOK WHERE Genre = vGenre;
-SELECT COUNT(*) INTO vBorrowCount FROM BOOK WHERE Genre = vGenre AND book_serial_number NOT IN (SELECT book_serial_number FROM Borrows);
+SELECT COUNT(*) INTO vTotalCount FROM Book WHERE Genre = vGenre;
+SELECT COUNT(*) INTO vBorrowCount FROM Book WHERE Genre = vGenre AND book_serial_number NOT IN (SELECT book_serial_number FROM Borrows);
 RETURN CONCAT(vBorrowCount,'/',vTotalCount);
 END//
 DELIMITER ;
@@ -43,3 +43,35 @@ UPDATE Activity SET start_time =
     END;
 
 SELECT* FROM Activity; 
+DROP PROCEDURE IF EXISTS borrowBook;
+DELIMITER //
+CREATE PROCEDURE borrowBook(userid CHAR(5), ptitle CHAR(255), pauthor CHAR(255))
+    BEGIN
+    DECLARE targetBook char(255);
+
+	SELECT book_serial_number INTO targetBook FROM Book WHERE title = ptitle AND author = pauthor;
+    
+    INSERT INTO Borrows (user_id, book_serial_number, borrow_status)
+    SELECT userid, targetBook, 'borrowed'
+    WHERE NOT EXISTS (
+		SELECT 1 FROM Borrows WHERE book_serial_number = targetBook
+    );
+    END //
+DELIMITER ;
+
+CALL borrowBook('00001', 'Dune', 'Frank Herbert');
+SELECT b.book_serial_number, br.borrow_status
+FROM Book b
+JOIN Borrows br ON b.book_serial_number = br.book_serial_number
+WHERE b.title = 'Dune'
+AND br.user_id = '00001';
+
+DELETE FROM Borrows
+WHERE book_serial_number IN (
+    SELECT b.book_serial_number
+    FROM Book b
+    WHERE b.title = 'Dune'
+      AND b.author = 'Frank Herbert'
+)
+AND user_id = '00001';
+
